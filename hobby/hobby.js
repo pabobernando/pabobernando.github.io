8e8c8a436f2558e9fd58235e4d7263dc0d2846c3
pabobernando <pabobernando69@gmail.com>
2022-01-04 14:03:27 +0700

    vantaJS

--- hobby/hobby.js
+++ hobby/hobby.js
@@ -3,3 +3,18 @@ VanillaTilt.init(document.querySelectorAll(".text-box"),{
     reverse:true,
     "max-glare":0.5
   })
+
+  VANTA.NET({
+    el: "#hobby",
+    mouseControls: true,
+  touchControls: true,
+  gyroControls: false,
+  minHeight: 200.00,
+  minWidth: 200.00,
+  scale: 1.00,
+  scaleMobile: 1.00,
+  color: 0x15eded,
+  backgroundColor: 0x0,
+  points: 12.00,
+  maxDistance: 21.00
+  })
